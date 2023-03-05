0a86b8e12e6f447f530154fb4789a63783a4af22
Andrei K <kavalerand@gmail.com>
2023-03-05 13:15:40 +0300

    added task 47, 50 and 53

---
+++ Task50/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 50. Напишите программу, которая на вход 
+//принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+int numberRows = Promt("Введите количество строк в двумерном массиве: ");
+int numberCols = Promt("Введите количество столцов в двумерном массиве: ");
+int[,] matrixInt = CreateMatrixDouble(numberRows, numberCols);
+Console.WriteLine();
+Console.WriteLine("Ваш массив:");
+PrintArray(matrixInt);
+int numberFindRows = Promt($"Введите от 0 до {numberRows - 1} номер строки искомого элемента массива: ");
+int numberFindCols = Promt($"Введите от 0 до {numberCols - 1} номер столбца искомого элемента массива: ");
+bool resultFind = ResultFind(matrixInt, numberFindRows, numberFindCols);
+Console.WriteLine();
+if (resultFind == true) 
+Console.WriteLine($"На позиции [{numberFindRows}, {numberFindCols}] находится элемент со значением ->" 
++ matrixInt[numberFindRows, numberFindCols]);
+else Console.WriteLine($"На позиции [{numberFindRows}, {numberFindCols}] элемента НЕ существует");
+
+// методы
+int[,] CreateMatrixDouble(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Random rnd = new Random();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next();
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],10} " + "|");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool ResultFind(int[,] matrix, int numRow, int numCol)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (numRow < matrix.GetLength(0) && numCol < matrix.GetLength(1))
+                return true;
+        }
+    }
+    return false;
+}
---
+++ Task53/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int numberRows = Promt("Введите количество строк в двумерном массиве: ");
+int numberCols = Promt("Введите количество столцов в двумерном массиве: ");
+int[,] matrixInt = CreateMatrixInt(numberRows, numberCols);
+Console.WriteLine();
+Console.WriteLine("Ваш массив:");
+PrintMatrix(matrixInt);
+double[] getArrayAverage = CreateMatrixAverageCols(matrixInt);
+Console.WriteLine();
+PrintArray (getArrayAverage);
+
+// методы
+int[,] CreateMatrixInt(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Random rnd = new Random();
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],10} " + "|");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+double[] CreateMatrixAverageCols(int[,] matrix)
+{
+    int number = matrix.GetLength(1);
+    double[] matrixAverage = new double[number];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+        matrixAverage[j] += matrix[i, j];
+         
+        }
+        matrixAverage[j] = Math.Round(matrixAverage[j] / matrix.GetLength(0), 1, MidpointRounding.ToZero);
+    }
+    
+    return matrixAverage;
+}
+
+void PrintArray(double[] array)
+{
+    Console.WriteLine("Среднее арифметическое элементов в каждом столбце Ваше массива:");
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j], 10} | ");
+    }
+}
